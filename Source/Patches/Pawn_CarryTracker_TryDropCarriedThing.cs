403737e7b512eaaa51ee4c4547162a5e9c3bb169
tenguin <73805320+tenguin@users.noreply.github.com>
2021-08-07 15:41:01 -0400

    1.3
    Added Strip after haul option

---
+++ Source/Patches/Pawn_CarryTracker_TryDropCarriedThing.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Verse;
+using System;
+
+namespace AutoStripOnHaul
+{
+    [HarmonyPatch(typeof(Pawn_CarryTracker), "TryDropCarriedThing",
+        new Type[] { typeof(IntVec3), typeof(ThingPlaceMode), typeof(Thing), typeof(Action<Thing, int>) },
+        new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Normal })]
+    internal static class Pawn_CarryTracker_TryDropCarriedThing
+    {
+        [HarmonyPrefix]
+        private static void TryDropCarriedThing(ref Pawn_CarryTracker __instance)
+        {
+            if (Settings.StripAfterHaul)
+            {
+                Thing thing = __instance.CarriedThing;
+                StripUtility.StripAll(ref thing);
+            }
+        }
+    }
+}
